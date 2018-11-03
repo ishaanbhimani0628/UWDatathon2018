956fd6ce1b74ae0c122b839cb2b1526e1d8b2e11
ishaanbhimani0628 <ishaanb@uw.edu>
2018-11-02 19:20:52 -0700

    add graph of pledge per proj per cat

--- data_exploration_ishaan.py
+++ data_exploration_ishaan.py
@@ -2,6 +2,8 @@ import pandas
 import numpy
 import sklearn
 import matplotlib.pyplot as plt
+import math
+import json
 
 
 #Initial wrangling
@@ -15,10 +17,82 @@ import matplotlib.pyplot as plt
 
 #Working with the new dataset:
 kickstart_data = pandas.read_csv('kickstarter_dataset_edited.csv')
-#plt.scatter(kickstart_data['usd_goal_real'], kickstart_data['usd pledged']) #tells us that don't set goal above 2million
-# pandas.scatter_matrix(kickstart_data) #only plots numerical, not as useful
-#plt.show()
-#kickstart_data['category'].value_counts().to_csv('category.csv')
-#kickstart_data['main_category'].value_counts().to_csv('main_category.csv')
-#Plot categories vs usd_pledged
-#Goals vs usd_pledged
+#kickstart_data.info()
+null_array = []
+median_usd_pledge = kickstart_data['usd pledged'].median()
+kickstart_data['usd pledged'].fillna(median_usd_pledge, inplace = True)
+for index , row in kickstart_data.iterrows():
+    if row['main_category'] != 
+#
+# value_counts = kickstart_data['main_category'].value_counts()
+# total_pledged_categories = {
+#     'Film & Video': 0,
+#     'Music': 0,
+#     'Publishing': 0,
+#     'Games': 0,
+#     'Technology': 0,
+#     'Design': 0,
+#     'Art': 0,
+#     'Food': 0,
+#     'Fashion': 0,
+#     'Theater': 0,
+#     'Comics': 0,
+#     'Photography': 0,
+#     'Crafts': 0,
+#     'Journalism': 0,
+#     'Dance': 0,
+# }
+# for index, row in kickstart_data.iterrows():
+#     main_category = row['main_category']
+#     pledge = row['usd pledged']
+#     #print(pledge)
+#     total_pledged_categories[main_category] = total_pledged_categories[main_category] + pledge
+#
+# #print(total_pledged_categories)
+# pledged_per_categories = {
+#     'Film & Video': 0,
+#     'Music': 0,
+#     'Publishing': 0,
+#     'Games': 0,
+#     'Technology': 0,
+#     'Design': 0,
+#     'Art': 0,
+#     'Food': 0,
+#     'Fashion': 0,
+#     'Theater': 0,
+#     'Comics': 0,
+#     'Photography': 0,
+#     'Crafts': 0,
+#     'Journalism': 0,
+#     'Dance': 0,
+# }
+# projects_per_categories = {
+#     'Film & Video': 63585,
+#     'Music': 51918,
+#     'Publishing': 39874,
+#     'Games': 35231,
+#     'Technology': 32569,
+#     'Design': 30070,
+#     'Art': 28153,
+#     'Food': 24602,
+#     'Fashion': 22816,
+#     'Theater': 10913,
+#     'Comics': 10819,
+#     'Photography': 10779,
+#     'Crafts': 8809,
+#     'Journalism': 4755,
+#     'Dance': 3768,
+# }
+# for key in pledged_per_categories:
+#     pledged_per_categories[key] = total_pledged_categories[key]/projects_per_categories[key]
+# print(pledged_per_categories)
+# with open ('pledge_proj_cat.json', 'w') as fp:
+#     json.dump(pledged_per_categories, fp, indent=4)
+#
+# #plt.scatter(kickstart_data['usd_goal_real'], kickstart_data['usd pledged']) #tells us that don't set goal above 2million
+# # pandas.scatter_matrix(kickstart_data) #only plots numerical, not as useful
+# #plt.show()
+# #kickstart_data['category'].value_counts().to_csv('category.csv')
+# #kickstart_data['main_category'].value_counts().to_csv('main_category.csv')
+# #Plot categories vs usd_pledged
+# #Goals vs usd_pledged
